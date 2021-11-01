a8ea5c6d76ba2dd55e5b25c6d292072b6e6d6f75
manojeluri <manojeluri21@gmail.com>
2021-11-01 14:52:33 +0530

    input function

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,11 +1,13 @@
 var http = require('http');
-var url = "http://api.openweathermap.org/data/2.5/weather?q=New+Delhi,IN&appid=d023af95a70b814ef43150e52112910a&units=metric"
+var url = "http://api.openweathermap.org/data/2.5/weather?q=New+York,US&appid=d023af95a70b814ef43150e52112910a&units=metric"
 var server = http.createServer(function(request, response){
 //All logic goes here
     var request = require('request');
     request(url, function(err, res, body){
         var data = JSON.parse(body);
         response.write("<html><body><div id = 'container'>");
+        response.write("<p>Enter city: "+"<input type = 'text' name='city'>"+"</p>");
+        response.write("<input type = 'submit'>");
         response.write("<h1>"+'City Name - :'+data['name']+"<br>"+"</h1>")
         response.write("<h2>"+"Temperature - :"+data.main['temp']+"<br>"+"</h2>");
         response.write("<h2>"+"Sunset time - "+new Date(data.sys['sunset']*1000)+"</h2>");
