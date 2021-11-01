cf4e3c9a2361934f19fa9c416108556e3d9710f5
manojeluri <manojeluri21@gmail.com>
2021-11-01 17:39:23 +0530

    router file

--- basicLoginSystem/server.js
+++ basicLoginSystem/server.js
@@ -13,3 +13,15 @@ app.set('view engine', 'ejs');
 //load static assets
 app.use('/static',express.static(path.join(__dirname,'public')))
 app.use('/assets',express.static(path.join(__dirname,'public/assets')))
+app.use(session({
+    secret: uuidv4(), //  '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/route', router);
+// home route
+app.get('/', (req, res) =>{
+    res.render('base', { title : "Login System"});
+})
+app.listen(port, ()=>{ console.log("Lostening to the server on http://localhost:3000")});
