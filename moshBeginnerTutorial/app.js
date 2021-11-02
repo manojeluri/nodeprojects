6b31c35c20514f54d96c25ec4c12973badd74a62
manojeluri <manojeluri21@gmail.com>
2021-11-02 15:41:08 +0530

    os module

--- moshBeginnerTutorial/app.js
+++ moshBeginnerTutorial/app.js
@@ -1,3 +1,9 @@
-const path = require('path');
-var pathObj = path.parse(__filename);
-console.log(pathObj);
+const os = require('os');
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+
+//template string
+//ES6 /ES2015 : ECMAScript 6
+console.log(`Total memory: ${totalMemory}`);
+console.log(`Free memory: ${freeMemory}`);
