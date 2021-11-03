66a91a75d8fa7efb2943b34b684acfe5274e9f74
manojeluri <manojeluri21@gmail.com>
2021-11-03 15:54:59 +0530

    delete a member

--- ExpressJSCrashCource/routes/api/members.js
+++ ExpressJSCrashCource/routes/api/members.js
@@ -52,4 +52,16 @@ router.put('/:id',(req,res)=>{
     
 });
 
+//Delete single member
+router.delete('/:id',(req,res)=>{
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found){
+        res.json({msg:"Member deleted",members:members.filter(member => member.id !== parseInt(req.params.id))});
+    }
+    else{
+        res.status(400).json({msg:`No member with id of ${req.params.id}`});
+    }
+    
+});
+
 module.exports = router;
