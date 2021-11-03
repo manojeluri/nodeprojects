8a165027b002db2f989c49cc71d4ef4759a72056
manojeluri <manojeluri21@gmail.com>
2021-11-03 13:36:24 +0530

    middleware

--- ExpressJSCrashCource/index.js
+++ ExpressJSCrashCource/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const members = require('./members');
 const path = require('path');
+const logger = require('./middleware/logger');
+
 //init express
 const app = express();
 
-//create your endpoints/route handlers
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname,'public','index.html'));
+
+//Init middleware
+//app.use(logger);
+//creating a simple route(gets all members)
+app.get('/api/members', (req, res)=>{
+    res.json(members);
 });
+//Get a single member
+app.get('/api/members/:id',(req,res)=>{
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found){
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    }
+    else{
+        res.status(400).json({msg:`No member with id of ${req.params.id}`});
+    }
+    
+});
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
 PORT = process.env.PORT || 5000;
 //listen on a port
 app.listen(PORT,() => console.log(`Server started on ${PORT}`));
---
+++ ExpressJSCrashCource/members.js
@@ -0,0 +1,22 @@
+//array of users
+const members = [
+    {
+        id:1,
+        name:'John Doe',
+        email:'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id:2,
+        name:'Bob Williams',
+        email:'bob@gmail.com',
+        status:'inactive'
+    },
+    {
+        id:3,
+        name:'Manoj Eluri',
+        email:'manoj@gmail.com',
+        status:'active'
+    }
+];
+module.exports = members;
