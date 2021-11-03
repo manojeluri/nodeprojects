3a770f8774fb40e38fde8ae723e79f7eddc676f1
manojeluri <manojeluri21@gmail.com>
2021-11-03 15:31:20 +0530

    adding a member

--- ExpressJSCrashCource/index.js
+++ ExpressJSCrashCource/index.js
@@ -1,31 +1,18 @@
 const express = require('express');
-const members = require('./members');
 const path = require('path');
 const logger = require('./middleware/logger');
 
 //init express
 const app = express();
-
-
-//Init middleware
 //app.use(logger);
-//creating a simple route(gets all members)
-app.get('/api/members', (req, res)=>{
-    res.json(members);
-});
-//Get a single member
-app.get('/api/members/:id',(req,res)=>{
-    const found = members.some(member => member.id === parseInt(req.params.id));
-    if (found){
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    }
-    else{
-        res.status(400).json({msg:`No member with id of ${req.params.id}`});
-    }
-    
-});
+
+//bodyparser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 //set static folder
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/api/members',require('./routes/api/members'))
 PORT = process.env.PORT || 5000;
 //listen on a port
 app.listen(PORT,() => console.log(`Server started on ${PORT}`));
