fef1de9e1d72a9c6a590a11d9c72ea3016b26f38
manojeluri <manojeluri21@gmail.com>
2021-11-03 15:47:41 +0530

    update member

--- ExpressJSCrashCource/routes/api/members.js
+++ ExpressJSCrashCource/routes/api/members.js
@@ -19,7 +19,7 @@ router.get('/:id',(req,res)=>{
     }
     
 });
-//create member
+//Create member
 router.post('/',(req, res)=>{
     var newMember = {
         id: uuid.v4(),
@@ -32,6 +32,24 @@ router.post('/',(req, res)=>{
     }
     members.push(newMember);
     res.json(members);
+});
+//Update member
+router.put('/:id',(req,res)=>{
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if (found){
+        const updMember = req.body;
+        members.forEach(member =>{
+            if(member.id === parseInt(req.params.id)){
+                member.name = updMember.name ? updMember.name: member.name;
+                member.email= updMember.email ? updMember.email: member.email;
+            }
+            res.json({msg:"Member updated", member});
+        })
+    }
+    else{
+        res.status(400).json({msg:`No member with id of ${req.params.id}`});
+    }
+    
 });
 
 module.exports = router;
