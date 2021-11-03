12f935105fe92a4a4703ffaf8d85e262ef5c1de3
manojeluri <manojeluri21@gmail.com>
2021-11-03 16:23:40 +0530

    express handlebars

--- ExpressJSCrashCource/index.js
+++ ExpressJSCrashCource/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
 const logger = require('./middleware/logger');
+const exphbs = require('express-handlebars');
+const members = require('./Members');
 
 //init express
 const app = express();
+
 //app.use(logger);
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars')
 
 //bodyparser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+//Homepage route
+app.get('/',(req, res) => res.render('index',{title:'Member App',  members}));
+
 //set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
