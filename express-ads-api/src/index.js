d817dbdfc21e622ba7560380216bc12e16bd39b0
manojeluri <manojeluri21@gmail.com>
2021-11-08 15:24:24 +0530

    mongo.js

--- express-ads-api/src/index.js
+++ express-ads-api/src/index.js
@@ -9,3 +9,29 @@ const morgan = require('morgan');
 const app = express();
 
 //defining an array to work as a database (temporary solution)
+const ads = [
+    {title: "Hello World Again!"}
+];
+
+//adding helmet to enhance the api security
+app.use(helmet());
+
+//adding bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+//enabling cors for all requests
+app.use(cors());
+
+//adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+//defining an endpoint to return all ads
+app.get('/',(req,res)=>{
+    res.send(ads);
+});
+
+//starting the server
+app.listen(3001, ()=>{
+    console.log("Listening on port 3001");
+});
+
