5020d31d095544455f70182ae15a3a6a823dd9c9
manojeluri <manojeluri21@gmail.com>
2021-11-18 10:25:33 -0500

    mongoose setup

--- task-manager/app.js
+++ task-manager/app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config()
 //json middleware
 app.use(express.json());
 //routes
 app.get('/', (req,res)=>{
     res.send("Hello task manager");
-}).listen(port, console.log(`Listening on port ${port}`));
+});
 
 //all the routes
 // app.get('/api/v1/tasks') - get all the tasks
@@ -17,3 +19,13 @@ app.use('/api/v1/tasks', tasks);
 // app.get('/api/v1/tasks/:id') - get single task
 // app.patch('/api/v1/tasks/:id') - update tasks
 // app.delete('/api/v1/tasks/:id') - delete task
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Listening on port ${port}`))
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+start()
